d6bb2dcf92cde0d261ab5ab02024f50c64cd383b
Tessa Murray <tmurray@bsd220.org>
2024-12-18 20:19:04 -0800

    Initial Commit

---
+++ Country Flag Game/Country_Flag_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Country_Flag_GameApp.swift
+//  Country Flag Game
+//
+//  Created by Tessa Murray on 12/18/24.
+//
+
+import SwiftUI
+
+@main
+struct Country_Flag_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

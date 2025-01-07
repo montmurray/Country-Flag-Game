c24e3fca447b10c56ea244f9960150b32f7d7894
Tessa Murray <tmurray@bsd220.org>
2025-01-07 07:59:13 -0800

    Added Images and Q&A structs.

---
+++ Country Flag Game/Models/Question.swift
@@ -0,0 +1,20 @@
+//
+//  Question.swift
+//  Country Flag Game
+//
+//  Created by Tessa Murray on 1/7/25.
+//
+
+import Foundation
+
+struct Answer: Identifiable {
+    var id = UUID()
+    var text: String
+    var isCorrect: Bool
+}
+
+struct Question: Identifiable {
+    var id = UUID()
+    var correctAnswer: Answer
+    var incorrectAnswers: [Answer]
+}
